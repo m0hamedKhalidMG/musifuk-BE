f4089c8d21c61650b3e0d73ec6c406d7796a5ac9
m0hamedKhalidMG <mk9636736@gmail.com>
2024-05-04 23:07:10 +0300

    add auth

---
+++ server.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+require("dotenv").config({ path: ".env" });
+
+// Connect to our Database and handle any bad connections
+// mongoose.connect(process.env.DATABASE);
+
+mongoose.connect(process.env.DATABASE, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
+mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
+mongoose.connection.on("error", (err) => {
+  console.error(`🚫 Error → : ${err.message}`);
+});
+
+const glob = require("glob");
+const path = require("path");
+
+ glob.sync("./models/*.js").forEach(function (file) {
+   require(path.resolve(file));
+ });
+
+// Start our app!
+const app = require("./app");
+app.set("port", process.env.PORT || 80);
+const server = app.listen(app.get("port"), () => {
+  console.log(`Express running → On PORT : ${server.address().port}`);
+});
