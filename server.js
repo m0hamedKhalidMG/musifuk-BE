68c6bb89cf11c97257986ee97e2a62978c49a23e
m0hamedKhalidMG <115906020+m0hamedKhalidMG@users.noreply.github.com>
2024-06-26 23:58:44 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 require("dotenv").config({ path: ".env" });
 require('./models/ambulance/driver.js'); 
 require('./models/ambulance/ambulanceCar.js'); 
+require('./models/ambulance/requestsCar.js'); 
 
 // Connect to our Database and handle any bad connections
 // mongoose.connect(process.env.DATABASE);
