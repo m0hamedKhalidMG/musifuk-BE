3aaae2274df495065b973b98b195be1f144dea57
m0hamedKhalidMG <115906020+m0hamedKhalidMG@users.noreply.github.com>
2024-06-28 21:32:13 +0300

    Update socketServer.js

--- socketServer.js
+++ socketServer.js
@@ -1,109 +1,7 @@
-const Pushee = require("pusher");
-const mongoose = require("mongoose");
-const Driver = mongoose.model("Driver");
-const AmbulanceCar = mongoose.model("AmbulanceCar");
-const RequestsCar = mongoose.model("RequestsCar");
-
-
-let pusher;
-
-function initializePusher() {
-  pusher = new Pushee({
-    appId: "1826287",
-    key: "ea80cde6e77453dc1bf5",
-    secret: "c60a75a7c8986c98f0a5",
-    cluster: "mt1",
-    useTLS: true,
-  });
-
-  return pusher;
-}
-async function assignCarToRequest  (req, res)  {
-  const { requestId, carIds } = req.body;
-  const cars = await AmbulanceCar.find({ _id: { $in: carIds } });
-  if (cars.length !== carIds.length) {
-    return res.status(400).json({ error: "One or more cars not found" });
-  }
-  const unavailableCars = cars.filter((car) => car.status !== "available");
-  if (unavailableCars.length > 0) {
-    return res
-      .status(400)
-      .json({ error: "One or more cars are not available" });
-  }
-
-  const updatedRequest = await RequestsCar.findByIdAndUpdate(
-    requestId,
-    { $push: { assignedCars: { $each: carIds } } },
-    { new: true }
-  ).populate("assignedCars");
-
-  if (!updatedRequest) {
-    return res.status(404).json({ error: "Ambulance request not found" });
-  }
-
-  await AmbulanceCar.updateMany(
-    { _id: { $in: carIds } },
-    { $set: { status: "busy", deliveryStatus: "on progress" } }
-  );
-
-
-  carIds.forEach((carId) => {
-    pusher.trigger(`car_${carId}`, "newassignment", {
-      message: "You have a new assignment",
-      request: updatedRequest,
-    });
-  });
-  res.json(updatedRequest);
-};
-async function updatePosition(req,res) {
-  try {
-
-    if (!req.user || !req.user._id) {
-      throw new Error("User not authenticated or missing user ID");
-    }
-
-    const ambulanceCar = await AmbulanceCar.findOne({ assignedDriver: req.user._id });
-
-
-    console.log(ambulanceCar)
-    if (!ambulanceCar) {
-      throw new Error("Car not found in the AmbulanceCar collection");
-    }
-    const { newCoordinates, newTimestamp } = req.body;
-    if (
-      !Array.isArray(newCoordinates) ||
-      newCoordinates.length !== 2 ||
-      !newCoordinates.every(Number.isFinite)
-    ) {
-      throw new Error("newCoordinates must be an array of two numbers");
-    }
-    ambulanceCar.lastLocation.coordinates.coordinates = newCoordinates;
-    ambulanceCar.lastLocation.timestamp = newTimestamp;
-
-    await ambulanceCar.save();
-    const carNumber=ambulanceCar.carNumber
-    pusher.trigger(`car_${carNumber}`, "positionUpdated", {
-      message: "Position updated",
-      coordinates: newCoordinates,
-    });
-    return res.json({ message: "last location updated successfully" });
-  } catch (error) {
-    console.error("Error updating last location:", error);
-    throw error;
-  }
-};
-
-module.exports = {
-  initializePusher,
-  updatePosition,
-  assignCarToRequest
-};
 const Pusher = require("pusher");
 const mongoose = require("mongoose");
-const Driver = mongoose.model("Driver");
 const AmbulanceCar = mongoose.model("AmbulanceCar");
 const RequestsCar = mongoose.model("RequestsCar");
-
 
 let pusher;
 
@@ -118,7 +16,8 @@ function initializePusher() {
 
   return pusher;
 }
-async function assignCarToRequest  (req, res)  {
+
+async function assignCarToRequest(req, res) {
   const { requestId, carIds } = req.body;
   const cars = await AmbulanceCar.find({ _id: { $in: carIds } });
   if (cars.length !== carIds.length) {
@@ -146,29 +45,30 @@ async function assignCarToRequest  (req, res)  {
     { $set: { status: "busy", deliveryStatus: "on progress" } }
   );
 
-
   carIds.forEach((carId) => {
     pusher.trigger(`car_${carId}`, "newassignment", {
       message: "You have a new assignment",
       request: updatedRequest,
     });
   });
-  res.json(updatedRequest);
-};
-async function updatePosition(req,res) {
-  try {
 
+  res.json(updatedRequest);
+}
+
+async function updatePosition(req, res) {
+  try {
     if (!req.user || !req.user._id) {
       throw new Error("User not authenticated or missing user ID");
     }
 
-    const ambulanceCar = await AmbulanceCar.findOne({ assignedDriver: req.user._id });
+    const ambulanceCar = await AmbulanceCar.findOne({
+      assignedDriver: req.user._id,
+    });
 
-
-    console.log(ambulanceCar)
     if (!ambulanceCar) {
       throw new Error("Car not found in the AmbulanceCar collection");
     }
+
     const { newCoordinates, newTimestamp } = req.body;
     if (
       !Array.isArray(newCoordinates) ||
@@ -177,24 +77,27 @@ async function updatePosition(req,res) {
     ) {
       throw new Error("newCoordinates must be an array of two numbers");
     }
+
     ambulanceCar.lastLocation.coordinates.coordinates = newCoordinates;
     ambulanceCar.lastLocation.timestamp = newTimestamp;
 
     await ambulanceCar.save();
-    const carNumber=ambulanceCar.carNumber
+
+    const carNumber = ambulanceCar.carNumber;
     pusher.trigger(`car_${carNumber}`, "positionUpdated", {
       message: "Position updated",
       coordinates: newCoordinates,
     });
+
     return res.json({ message: "last location updated successfully" });
   } catch (error) {
     console.error("Error updating last location:", error);
     throw error;
   }
-};
+}
 
 module.exports = {
   initializePusher,
   updatePosition,
-  assignCarToRequest
+  assignCarToRequest,
 };
